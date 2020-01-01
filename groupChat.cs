b92c2b6a887f7d8f58538f9bb2d4e9d08ed42274
e <1183936418@qq.com>
2020-01-02 03:45:33 +0800

    取消可能引发异常的输入框失焦/得焦提示机制。

--- groupChat.cs
+++ groupChat.cs
@@ -28,29 +28,6 @@ namespace onlineChat
             chatGroupName.Text = name;
             DrawMessage();
             drawList();
-            inputBox.ForeColor = Color.Gray;
-            inputBox.Text = "此处输入文字消息......";
-            inputBox.LostFocus += new EventHandler(this.inputTip);  //消息输入框失焦
-            inputBox.Enter += new EventHandler(this.inputTipCancel);  //消息输入框得到焦点
-        }
-
-        private void inputTipCancel(object sender, EventArgs e)  //消息框得到焦点取消文字提示
-        {
-            if (inputBox.ForeColor == Color.Gray)
-            {
-                inputBox.ForeColor = Color.Black;
-                inputBox.Text = "";
-            }
- 
-        }
-
-        private void inputTip(object sender, EventArgs e)  //消息框失焦提示文字
-        {
-            if (inputBox.Text == "")
-            {
-                inputBox.ForeColor = Color.Gray;
-                inputBox.Text = "此处输入文字消息......";
-            }
         }
 
         public void DrawMessage()
